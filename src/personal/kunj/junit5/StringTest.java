4f224e8e4237d97a62f7cab318a3dcf3021f147b
KB <kb@kb.com>
2019-11-08 23:17:17 +0530

    Step 08 - Setting up database connections - @BeforeAll, @AfterAll

--- src/personal/kunj/junit5/StringTest.java
+++ src/personal/kunj/junit5/StringTest.java
@@ -5,12 +5,24 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Initialize connection to the database");
+	}
+
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Closing connection to the database");
+	}
 
 	// @Before --> In JUnit 4
 	@BeforeEach
