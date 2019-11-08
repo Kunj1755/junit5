f31f062ce16d0667237c77c12eeb7e279eef6bfb
KB <kb@kb.com>
2019-11-08 20:07:12 +0530

    Step 05 - Writing JUnit Assertions - assertNull and assertTrue

--- src/personal/kunj/junit5/StringTest.java
+++ src/personal/kunj/junit5/StringTest.java
@@ -1,5 +1,6 @@
 package personal.kunj.junit5;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -7,10 +8,27 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
 	@Test
-	void test() {
+	void length_basic() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
 		assertEquals(expectedLength, actualLength);
+	}
+
+	@Test
+	void toUpperCase() {
+		String actualString = "kunj".toUpperCase();
+		String expectedString = "KUNJ";
+		assertNotNull(actualString);
+		assertEquals(expectedString, actualString);
+	}
+
+	// Assserting boolean
+	@Test
+	void contains_basic() {
+		String str = "abcdef";
+		boolean result = str.contains("try");
+		assertEquals(false, result); // same as assertFalse(result);
+		// assertTrue
 	}
 
 }
