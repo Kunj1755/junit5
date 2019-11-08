91d328d750701872397c2f1d6eb76787e7366411
KB <kb@kb.com>
2019-11-08 19:36:39 +0530

    project with demo test file created

---
+++ src/personal/kunj/junit5/StringTest.java
@@ -0,0 +1,14 @@
+package personal.kunj.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
